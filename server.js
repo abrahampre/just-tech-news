4e1820655ef81e09944e88b12b12f638637f2234
abraham perez reyes <abrahampre27@gmail.com>
2021-09-22 14:24:53 -0500

    models comment

--- server.js
+++ server.js
@@ -1,21 +1,29 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequalize = require('./config/connection');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs =exphbs.create({});
+
+
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
+//lecture 14
 app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,'public'))); //The express.static() method is a built-in Express.js middleware function that can take all of the contents of a folder and serve them as static assets. This is useful for front-end specific files like images, style sheets, and JavaScript files
+app.engine('handlebars', hbs.engine);
+app.set('view engine','handlebars')
 
-//turn on routes
 
 app.use(routes);
 
 //turn on connection to db and server
 
-sequalize.sync({force:false}).then(()=>{
+sequalize.sync({force:true}).then(()=>{
     app.listen(PORT,() => console.log('Now Listening'))
 })
 
