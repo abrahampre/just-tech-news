3a85e1c2cacfe969e49ddb04553a6843369b26e7
abraham perez reyes <abrahampre27@gmail.com>
2021-09-22 14:17:00 -0500

    fixxing comment section

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
-
+const commentRoutes = require('./comment-routes');
 const userRoutes = require ('./user-routes');
 const postRoutes = require('./post-routes');
 //basicamente estamos creando este file para meter todas las rutas por aqui
 
 router.use('/users',userRoutes);
 router.use('/posts', postRoutes);
-
+router.use('/comments', commentRoutes)
 
 module.exports=router;
 
